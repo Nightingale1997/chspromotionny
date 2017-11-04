86b09c3ff86bb28304529a1290e7c7026d992411
Nightingale1997 <kevin.solovjov@gmail.com>
2017-11-04 21:17:05 +0100

    Changes from Work

--- theme-chspromotion/process.php
+++ theme-chspromotion/process.php
@@ -1,6 +1,6 @@
 <?php
  
-    $to = "wowthemesnet@gmail.com"; 
+    $to = "kevin.solovjov@gmail.com"; 
     $from = $_REQUEST['email']; 
     $name = $_REQUEST['name']; 
     $headers = "From: $from"; 
